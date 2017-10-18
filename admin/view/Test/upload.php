90016dfa07c83a04e3300bf30335e3a03972883e
mgckid <654352323@qq.com>
2017-10-18 20:32:33 +0800

    网站改版

--- admin/view/Test/upload.php
+++ admin/view/Test/upload.php
@@ -1,11 +1,7 @@
 <?php $this->layout('Layout/admin'); ?>
-<!-- The jQuery UI widget factory, can be omitted if jQuery UI is already included -->
-<script src="/static/admin/js/jQuery-File-Upload-9.19.1/js/vendor/jquery.ui.widget.js"></script>
-<!-- The Iframe Transport is required for browsers without support for XHR file uploads -->
-<script src="/static/admin/js/jQuery-File-Upload-9.19.1/js/jquery.iframe-transport.js"></script>
-<!-- The basic File Upload plugin -->
-<script src="/static/admin/js/jQuery-File-Upload-9.19.1/js/jquery.fileupload.js"></script>
-<!-- Bootstrap JS is not required, but included for the responsive demo navigation -->
+<link href="/static/admin/js/webuploader-master/dist/webuploader.css" rel="stylesheet" type="text/css"/>
+<script type="text/javascript" src="/static/admin/js/webuploader-master/dist/webuploader.js"></script>
+
 
 <div class="panel panel-default">
     <div class="panel-body">
@@ -17,8 +13,16 @@
                     <label class="control-label col-sm-2"> 上传广告图</label>
 
                     <div class="col-sm-8">
-                        <input type="hidden" name="ad_image" id="ad_image"/>
-                        <input id="fileupload" type="file" name="files[]" multiple="">
+                       <span>
+                           <div id="uploader-demo">
+                               <!--用来存放item-->
+                               <div id="thelist" class="uploader-list"></div>
+                               <div>
+                                   <div id="filePicker">选择图片</div>
+                                   <a id="ctlBtn" class="btn btn-default">开始上传</a>
+                               </div>
+                           </div>
+                       </span>
                     </div>
                     <label class="col-sm-2"> </label>
                 </div>
@@ -36,27 +40,104 @@
 </div>
 
 <script>
-  //  $('#upload_file').fileupload();
-  $(function () {
-      // Change this to the location of your server-side upload handler:
-      var config = {
-          url: '<?=U('Upload/index')?>',
-          dataType: 'json',
-          done: function (e, data) {
-              $.each(data.result.files, function (index, file) {
-                  $('<p/>').text(file.name).appendTo('#files');
-              });
-          },
-          progressall: function (e, data) {
-              var progress = parseInt(data.loaded / data.total * 100, 10);
-              $('#progress .progress-bar').css(
-                  'width',
-                  progress + '%'
-              );
-          }
-      };
-      $('#fileupload').fileupload(config);
-  });
+    $(function () {
+        /*init webuploader*/
+        var $list = $("#thelist");   //这几个初始化全局的百度文档上没说明，好蛋疼。
+        var $btn = $("#ctlBtn");   //开始上传
+        var thumbnailWidth = 100;   //缩略图高度和宽度 （单位是像素），当宽高度是0~1的时候，是按照百分比计算，具体可以看api文档
+        var thumbnailHeight = 100;
+
+        var uploader = WebUploader.create({
+            // 选完文件后，是否自动上传。
+            auto: false,
+
+            // swf文件路径
+            swf: '/static/admin/js/webuploader-master/dist/Uploader.swf',
+
+            // 文件接收服务端。
+            server: '<?=U('Upload/index')?>',
+
+            // 选择文件的按钮。可选。
+            // 内部根据当前运行是创建，可能是input元素，也可能是flash.
+            pick: '#filePicker',
+
+            // 只允许选择图片文件。
+            accept: {
+                title: 'Images',
+                extensions: 'gif,jpg,jpeg,bmp,png',
+                mimeTypes: 'image/*'
+            },
+            method: 'POST',
+        });
+        // 当有文件添加进来的时候
+        uploader.on('fileQueued', function (file) {  // webuploader事件.当选择文件后，文件被加载到文件队列中，触发该事件。等效于 uploader.onFileueued = function(file){...} ，类似js的事件定义。
+            var $li = $(
+                    '<div id="' + file.id + '" class="file-item thumbnail">' +
+                    '<img>' +
+                    '<div class="info">' + file.name + '</div>' +
+                    '</div>'
+                ),
+                $img = $li.find('img');
+
+
+            // $list为容器jQuery实例
+            $list.append($li);
+
+            // 创建缩略图
+            // 如果为非图片文件，可以不用调用此方法。
+            // thumbnailWidth x thumbnailHeight 为 100 x 100
+            uploader.makeThumb(file, function (error, src) {   //webuploader方法
+                if (error) {
+                    $img.replaceWith('<span>不能预览</span>');
+                    return;
+                }
+
+                $img.attr('src', src);
+            }, thumbnailWidth, thumbnailHeight);
+        });
+        // 文件上传过程中创建进度条实时显示。
+        uploader.on('uploadProgress', function (file, percentage) {
+            var $li = $('#' + file.id),
+                $percent = $li.find('.progress span');
+
+            // 避免重复创建
+            if (!$percent.length) {
+                $percent = $('<p class="progress"><span></span></p>')
+                    .appendTo($li)
+                    .find('span');
+            }
+
+            $percent.css('width', percentage * 100 + '%');
+        });
+
+        // 文件上传成功，给item添加成功class, 用样式标记上传成功。
+        uploader.on('uploadSuccess', function (file) {
+            $('#' + file.id).addClass('upload-state-done');
+        });
+
+        // 文件上传失败，显示上传出错。
+        uploader.on('uploadError', function (file) {
+            var $li = $('#' + file.id),
+                $error = $li.find('div.error');
+
+            // 避免重复创建
+            if (!$error.length) {
+                $error = $('<div class="error"></div>').appendTo($li);
+            }
+
+            $error.text('上传失败');
+        });
+
+        // 完成上传完了，成功或者失败，先删除进度条。
+        uploader.on('uploadComplete', function (file) {
+            $('#' + file.id).find('.progress').remove();
+        });
+        $btn.on('click', function () {
+            console.log("上传...");
+            uploader.upload();
+            console.log("上传成功");
+        });
+    });
 </script>
 
 
