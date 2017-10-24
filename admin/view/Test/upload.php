4dd49557123c49a9f1417b6cc97f8af6bf901d85
mgckid <654352323@qq.com>
2017-10-24 17:49:23 +0800

    网站改版

--- admin/view/Test/upload.php
+++ admin/view/Test/upload.php
@@ -21,7 +21,6 @@
                                 <div class="container-box">
                                     <div class="add" id="choice_file">+</div>
                                 </div>
-                                <a href="#" class="btn btn-success" id="browse" >选择文件...</a>
                                 <a href="#" class="btn btn-info" id="upload-btn" >开始上传</a>
                             </div>
                             <ul id="file-list"></ul>
@@ -46,52 +45,79 @@
 
 <script>
     var browse_button = 'choice_file';
-    var uploader = new plupload.Uploader({
+    var config = {
         browse_button: browse_button, // this can be an id of a DOM element or the DOM element itself
         url: '<?=U('Upload/index')?>',
         multipart: true,//为true时将以multipart/form-data的形式来上传文件，为false时则以二进制的格式来上传文件。
         multipart_params: {source: 'upload'},//上传时的附加参数，
         max_retries: 0, //	当发生plupload.HTTP_ERROR错误时的重试次数，为0时表示不重试
         chunk_size: 0,   //	分片上传文件时，每片文件被切割成的大小，为数字时单位为字节。也可以使用一个带单位的字符串，如"200kb"。当该值为0时表示不使用分片上传功能
-        multi_selection: true, //	是否可以在文件浏览对话框中选择多个文件，true为可以，false为不可以。默认true，
+        multi_selection: false, //	是否可以在文件浏览对话框中选择多个文件，true为可以，false为不可以。默认true，
         unique_names: false,//当值为true时会为每个上传的文件生成一个唯一的文件名，
-        file_data_name: 'file'//指定文件上传时文件域的名称，默认为file,例如在php中你可以使用$_FILES['file']来获取上传的文件信息
-//        filters: {
-//            prevent_duplicates: true, //不允许选取重复文件
-//            mime_types: [ //只允许上传图片和zip文件
-//                {title: "Image files", extensions: "jpg,gif,png,jpeg,bmp"},
-//                {title: "Zip files", extensions: "zip"}
-//            ],
-//            max_file_size: '5000kb' //最大只能上传400kb的文件
-//        },
-//        flash_swf_url: '/static/admin/js/plupload-master/js/Moxie.swf',//flash上传组件的url地址
-//        silverlight_xap_url: '/static/admin/js/plupload-master/js/Moxie.xap'//silverlight上传组件的url地址
-//        resize: {},
-//        drop_element: '',
-//        required_features: '',
-//        container: ''//用来指定Plupload所创建的html结构的父容器，默认为前面指定的browse_button的父元素。
+        file_data_name: 'file',//指定文件上传时文件域的名称，默认为file,例如在php中你可以使用$_FILES['file']来获取上传的文件信息
+        /*不需要用到的属性 开始*/
+        /*flash_swf_url: '/static/admin/js/plupload-master/js/Moxie.swf',//flash上传组件的url地址
+        silverlight_xap_url: '/static/admin/js/plupload-master/js/Moxie.xap'//silverlight上传组件的url地址
+        filters: {
+            prevent_duplicates: true, //不允许选取重复文件
+            mime_types: [ //只允许上传图片和zip文件
+                {title: "Image files", extensions: "jpg,gif,png,jpeg,bmp"},
+                {title: "Zip files", extensions: "zip"}
+            ],
+            max_file_size: '5000kb' //最大只能上传400kb的文件
+        },
+        resize: {},
+        drop_element: '',
+        required_features: '',
+        container: '',//用来指定Plupload所创建的html结构的父容器，默认为前面指定的browse_button的父元素。*/
+        /*不需要用到的属性 结束*/
 
-    });
+        /*事件处理开始*/
+        init: {
+            //当Init事件发生后触发
+            PostInit: function (uploader) {
+                $('#upload-btn').click(function () {
+                    uploader.start();
+                })
+            },
+            //选择文件后触发
+            FilesAdded: function (uploader, files) {
+                var file_name = files[0].name; //文件名
+                if (!files[0] || !/image\//.test(files[0].type)) return; //确保文件是图片
+                $('#' + browse_button).hide(0)
+                var containerObj = $('#' + browse_button).parent('.container-box');
+                var preview_id = 'preview_' + files[0].id;
+                var progress_id = 'progress_' + files[0].id;
+                containerObj.append('<div id="' + preview_id + '" class="preview"></div>')
+                containerObj.append('<div id="' + progress_id + '" class="progress"></div>')
+                previewImage(files[0], function (image_source) {
+                    $('#' + preview_id).append('<image src="' + image_source + '"/>')
+                })
+            },
+            //当文件从上传队列移除后触发
+            FilesRemoved: function (uploader, files) {
+
+            },
+            //当队列中的某一个文件上传完成后触发
+            FileUploaded: function (uploader, file, responseObject) {
+
+            },
+            //当上传队列中所有文件都上传完成后触发
+            UploadComplete: function (uploader, files) {
+
+            },
+            //当发生错误时触发
+            Error: function (uploader, errObject) {
+
+            }
+        }
+        /*事件处理结束*/
+
+    };
+    var uploader = new plupload.Uploader(config);
     uploader.init();
-    uploader.bind('FilesAdded', function (uploader, files) {
-        var file_name = files[0].name; //文件名
-        if(!files[0] || !/image\//.test(files[0].type)) return; //确保文件是图片
-        $('#'+browse_button).hide(0)
-        var containerObj = $('#'+browse_button).parent('.container-box');
-        var preview_id = 'preview_'+files[0].id;
-        var progress_id = 'progress_'+files[0].id;
-        containerObj.append('<div id="' + preview_id + '" class="preview"></div>')
-        containerObj.append('<div id="' + progress_id + '" class="progress"></div>')
-        previewImage(files[0], function (image_source) {
-            $('#'+preview_id).append('<image src="'+image_source+'"/>')
-        })
-    });
 
-    function _build_preview(){
-
-    }
-
-
+    
     //plupload中为我们提供了mOxie对象
     //有关mOxie的介绍和说明请看：https://github.com/moxiecode/moxie/wiki/API
     //如果你不想了解那么多的话，那就照抄本示例的代码来得到预览的图片吧
@@ -138,9 +164,9 @@
         document.getElementById('console').innerHTML += "\nError #" + err.code + ": " + err.message;
     });*/
     //上传按钮
-    $('#upload-btn').click(function(){
-        uploader.start();
-    });
+//    $('#upload-btn').click(function(){
+//        uploader.start();
+//    });
 </script>
 
 
