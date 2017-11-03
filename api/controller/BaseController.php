29f0238629749909c791768e919fda26ed32e0aa
mgckid <654352323@qq.com>
2017-11-03 18:19:08 +0800

    网站改版

--- api/controller/BaseController.php
+++ api/controller/BaseController.php
@@ -16,13 +16,14 @@ class BaseController extends Controller
 {
     public function getRequestParam()
     {
-        if (ENVIRONMENT !== 'develop') {
-            $dex3 = new Dex3();
-            $encode_str = base64_decode($_REQUEST['param']);
-            $result = json_decode($dex3->decrypt($encode_str),true);
-        } else {
-            $result = $_REQUEST;
-        }
+//        if (ENVIRONMENT !== 'develop') {
+//            $dex3 = new Dex3();
+//            $encode_str = base64_decode($_REQUEST['param']);
+//            $result = json_decode($dex3->decrypt($encode_str),true);
+//        } else {
+//            $result = $_REQUEST;
+//        }
+        $result = $_REQUEST;
         return $result;
     }
 } 
